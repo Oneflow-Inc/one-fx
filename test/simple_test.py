6fea067a058aab7fe725294e05e0d51f6cc3f67e
AsakusaRinne <AsakusaRinne@gmail.com>
2022-12-21 08:24:46 +0000

    Init the framework (simple).

---
+++ test/simple_test.py
@@ -0,0 +1,44 @@
+import oneflow
+import sys
+sys.path.append(r'../one-fx')
+import fx
+
+fx.proxy.Proxy
+
+def test_tensor(t: oneflow.Tensor):
+    print(t)
+
+class MyModule(oneflow.nn.Module):
+    def __init__(self, do_activation : bool = False):
+        super().__init__()
+        self.do_activation = do_activation
+        self.linear = oneflow.nn.Linear(512, 512)
+        self.relu = oneflow.nn.ReLU()
+
+    def forward(self, x):
+        x = self.linear(x)
+
+        if self.do_activation:
+            x = self.relu(x)
+        return x
+
+without_activation = MyModule(do_activation=False)
+with_activation = MyModule(do_activation=True)
+
+traced_without_activation = fx.symbolic_trace(without_activation)
+print(traced_without_activation.code)
+"""
+def forward(self, x):
+    linear = self.linear(x);  x = None
+    return linear
+"""
+
+traced_with_activation = fx.symbolic_trace(with_activation)
+print(traced_with_activation.code)
+"""
+import oneflow
+def forward(self, x):
+    linear = self.linear(x);  x = None
+    relu = oneflow.relu(linear);  linear = None
+    return relu
+"""
