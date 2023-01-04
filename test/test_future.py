b19269f8c12353e6c94159d6a1d99bb9245f21a7
AsakusaRinne <AsakusaRinne@gmail.com>
2023-01-04 07:24:30 +0000

    Add more tests.

---
+++ test/test_future.py
@@ -0,0 +1,52 @@
+# Owner(s): ["module: fx"]
+
+from __future__ import annotations    # type: ignore[attr-defined]
+import oneflow
+import typing
+import sys
+sys.path.append(r'../one-fx')
+from fx import symbolic_trace
+
+class A:
+    def __call__(self, x: oneflow.Tensor):
+        return oneflow.add(x, x)
+
+# No forward references
+class M1(oneflow.nn.Module):
+    def forward(self, x: oneflow.Tensor, a: A) -> oneflow.Tensor:
+        return a(x)
+
+# Forward references
+class M2(oneflow.nn.Module):
+    def forward(self, x: 'oneflow.Tensor', a: 'A') -> 'oneflow.Tensor':
+        return a(x)
+
+# Non-oneflow annotation with no internal forward references
+class M3(oneflow.nn.Module):
+    def forward(self, x: typing.List[oneflow.Tensor], a: A) -> oneflow.Tensor:
+        return a(x[0])
+
+# Non-oneflow annotation with internal forward references
+class M4(oneflow.nn.Module):
+    def forward(self, x: typing.List['oneflow.Tensor'], a: A) -> 'oneflow.Tensor':
+        return a(x[0])
+
+x = oneflow.rand(2, 3)
+
+ref = oneflow.add(x, x)
+
+traced1 = symbolic_trace(M1())
+res1 = traced1(x, A())
+assert oneflow.all(oneflow.eq(ref, res1))
+
+traced2 = symbolic_trace(M2())
+res2 = traced2(x, A())
+assert oneflow.all(oneflow.eq(ref, res2))
+
+traced3 = symbolic_trace(M3())
+res3 = traced3([x], A())
+assert oneflow.all(oneflow.eq(ref, res3))
+
+traced4 = symbolic_trace(M4())
+res4 = traced4([x], A())
+assert oneflow.all(oneflow.eq(ref, res4))
---
+++ test/test_fx_param_shape_control_flow.py
@@ -0,0 +1,154 @@
+# Owner(s): ["module: fx"]
+
+import unittest
+import oneflow
+import sys
+sys.path.append(r'../one-fx')
+import fx
+import numpy as np
+
+class MyModuleBase(oneflow.nn.Module):
+    def forward(self, x):
+        matrx = self.get_mul_matrix()
+        if self.no_relu():
+            return oneflow._C.mm(x, matrx)
+        else:
+            return oneflow._C.relu(oneflow._C.mm(x, matrx))
+
+    def get_mul_matrix(self):
+        return self.param
+
+    def no_relu(self):
+        raise Exception("not implemented")
+
+class MyModuleParamShape(MyModuleBase):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.param = oneflow.nn.Parameter(oneflow.randn(in_channels, 3))
+
+    def no_relu(self):
+        return self.param.shape[0] < 10
+
+
+class MyModuleParamSize(MyModuleBase):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.param = oneflow.nn.Parameter(oneflow.randn(in_channels, 3))
+
+    def no_relu(self):
+        return self.param.size()[0] < 10
+
+
+class MyModuleParamDim(MyModuleBase):
+    def __init__(self, param):
+        super().__init__()
+        self.param = param
+
+    def get_mul_matrix(self):
+        return self.param[0] if (self.param.dim() == 3) else self.param
+
+    def no_relu(self):
+        return self.param.dim() == 3
+
+
+class MyModuleParamNDim(MyModuleBase):
+    def __init__(self, param):
+        super().__init__()
+        self.param = param
+
+    def get_mul_matrix(self):
+        return self.param[0] if (self.param.ndim == 3) else self.param
+
+    def no_relu(self):
+        return self.param.ndim == 3
+
+
+class MyModuleParamNumEl(MyModuleBase):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.param = oneflow.nn.Parameter(oneflow.randn(in_channels, 3))
+
+    def no_relu(self):
+        return self.param.numel() < 10 * 3
+
+
+
+class MyModuleParamNElement(MyModuleBase):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.param = oneflow.nn.Parameter(oneflow.randn(in_channels, 3))
+
+    def no_relu(self):
+        return self.param.nelement() < 10 * 3
+
+
+class TestConstParamShapeInControlFlow(unittest.TestCase):
+
+    def verify_mm_relu_mods(self, mm_only_mod, relu_mod):
+        """
+        Verify one module only does a mm op while the other
+        performs both mm and relu ops in cascade
+        """
+        x = oneflow.randn(10, 5)
+        assert np.allclose(mm_only_mod(x).numpy(), oneflow._C.mm(x, mm_only_mod.get_mul_matrix()).numpy())
+        tracer = fx.Tracer(param_shapes_constant=True)
+        traced_graph = tracer.trace(mm_only_mod)
+
+        # verify the graph module calculates the same result
+        graph_mod_mm = fx.GraphModule(mm_only_mod, traced_graph)
+        assert np.allclose(graph_mod_mm(x).numpy(), oneflow._C.mm(x, mm_only_mod.get_mul_matrix()).numpy())
+
+
+        # Make a new module with different parameter shape to go down the different
+        # code path
+        x = oneflow.randn(10, 15)
+        assert np.allclose(relu_mod(x).numpy(), oneflow._C.relu(oneflow._C.mm(x, relu_mod.get_mul_matrix())).numpy())
+
+        tracer2 = fx.Tracer(param_shapes_constant=True)
+        traced_graph2 = tracer2.trace(relu_mod)
+
+        # verify the graph module calculates the same result
+        graph_mod_relu = fx.GraphModule(relu_mod, traced_graph2)
+        assert np.allclose(graph_mod_relu(x).numpy(), oneflow._C.relu(oneflow._C.mm(x, relu_mod.get_mul_matrix())).numpy())
+
+
+        graph1_node_targets = [n.target for n in traced_graph.nodes]
+        graph2_node_targets = [n.target for n in traced_graph2.nodes]
+
+        # the second graph has an exta relu function call node
+        assert oneflow.mm in graph1_node_targets and oneflow.mm in graph2_node_targets
+        assert oneflow.relu not in graph1_node_targets and oneflow.relu in graph2_node_targets
+
+    def test_param_shape_const(self):
+        mymod = MyModuleParamShape(in_channels=5)
+        mymod2 = MyModuleParamShape(in_channels=15)
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+    def test_param_size_const(self):
+        mymod = MyModuleParamSize(in_channels=5)
+        mymod2 = MyModuleParamSize(in_channels=15)
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+    def test_param_dim_const(self):
+        mymod = MyModuleParamDim(oneflow.nn.Parameter(oneflow.randn(2, 5, 3)))
+        mymod2 = MyModuleParamDim(oneflow.nn.Parameter(oneflow.randn(15, 3)))
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+    def test_param_ndim_const(self):
+        mymod = MyModuleParamNDim(oneflow.nn.Parameter(oneflow.randn(2, 5, 3)))
+        mymod2 = MyModuleParamNDim(oneflow.nn.Parameter(oneflow.randn(15, 3)))
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+    def test_param_numel_const(self):
+        mymod = MyModuleParamNumEl(in_channels=5)
+        mymod2 = MyModuleParamNumEl(in_channels=15)
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+    def test_param_nelement_const(self):
+        mymod = MyModuleParamNElement(in_channels=5)
+        mymod2 = MyModuleParamNElement(in_channels=15)
+        self.verify_mm_relu_mods(mymod, mymod2)
+
+
+if __name__ == '__main__':
+    unittest.main()
